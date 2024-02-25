ba50883e7a14287dbba6e7cdf8dd0a7cf6d30b8f
Praise Obah <obahsomto@gmail.com>
2024-02-25 12:33:04 -0800

    app

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""app.py to connect to API"""
+import os
+from models import storage
+from app.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
