f6d5bf40d434c53d8c60e2be9476f9f358e8fa88
Praise Obah <obahsomto@gmail.com>
2024-02-26 04:31:47 -0800

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,16 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """displays an error page"""
+    return make_response(jsonify({'error': Not found'}), 404)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
