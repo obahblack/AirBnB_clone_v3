18d3ac1551d1d0d81ae8b53fbd67395bace93fe6
Praise Obah <obahsomto@gmail.com>
2024-02-26 05:39:28 -0800

    git push

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -42,7 +42,7 @@ def post_state():
         return make_response(jsonify({'error': 'Missing name'}), 400)
     state = State(**request.get_json())
     state.save()
-    return make_reponse(jsonify(state.to_dict()), 201)
+    return make_response(jsonify(state.to_dict()), 201)
 
 @app_views.route('/states/<string:state_id>', methods=['PUT'],
                  strict_slashes=False)
