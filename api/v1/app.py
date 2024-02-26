2d1e264040796f9b6096722654c62d18061390d2
Praise Obah <obahsomto@gmail.com>
2024-02-26 14:04:05 -0800

    hello pls pass

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,19 +6,22 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """displays an error page"""
-    return make_response(jsonify({'error': "Not found"}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
