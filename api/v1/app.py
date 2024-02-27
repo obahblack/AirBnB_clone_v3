fe84c17c15583be865899c8d5c38e6210e6319ca
Praise Obah <obahsomto@gmail.com>
2024-02-27 14:50:52 -0800

    git push

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
-from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+import os
 from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app.teardown_appcontext
+def teardown_appcontext(code):
+    """teardown cpntext"""
+    storage.close()
 
-
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
+@app.errorhandler(404)
+def page_not_found(error):
+    """page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    pass
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
