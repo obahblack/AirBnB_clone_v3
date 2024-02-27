c7f0ed5506ab266ae6093553034c90e6606c551b
Praise Obah <obahsomto@gmail.com>
2024-02-27 12:41:41 -0800

    changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,22 +4,19 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
-
 hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
+        "amenities": "Amenity",
+        "cities": "City",
+        "Places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+        }
 
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats', strict_slashes=False)
 def hbnbStats():
